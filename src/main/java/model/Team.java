672d91ad89fa9c3aaa3f283725bf14499f9004e1
fabriziopadulano <fabpadulano@studenti.unina.it>
2025-04-19 10:16:15 +0200

    refactor partecipante and team

--- src/main/java/model/Team.java
+++ src/main/java/model/Team.java
@@ -3,23 +3,44 @@ package model;
 import java.util.ArrayList;
 
 public class Team {
-    public String name;
-    ArrayList<Partecipante> partecipanti = new ArrayList<>();
-    ArrayList<Documento> documenti = new ArrayList<>();
+    private String name;
+    private ArrayList<Partecipante> partecipanti = new ArrayList<>();
+    private ArrayList<Documento> documenti = new ArrayList<>();
 
     public Team(String nome, Partecipante partecipante) {
-        this.name = nome;
-        this.partecipanti.add(partecipante);
+        this.setName(nome);
+        this.getPartecipanti().add(partecipante);
+        partecipante.setTeam(this);
     }
 
 
-    public Team(String nome, Piattaforma piattaforma) {
-        this.name = nome;
-        this.partecipanti.add(new Partecipante(piattaforma, this, ERuoloTeam.UNKNOWN));
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Partecipante> getPartecipanti() {
+        return partecipanti;
+    }
+
+    public ArrayList<Documento> getDocumenti() {
+        return documenti;
+    }
+
+    private void addDocumento(Documento documento) {
+        documenti.add(documento);
+    }
+
+    public void setPartecipanti(ArrayList<Partecipante> partecipanti) {
+        this.partecipanti = partecipanti;
     }
 
     public void caricaProgresso(Documento documento) {
         this.documenti.add(documento);
     }
+
 
 }
